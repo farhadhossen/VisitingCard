fd88a07b6311ad61a7646cb24455d0601c02563b
farhadhossen <imfarhad01@gmail.com>
2022-11-15 22:49:30 +0600

    intent added

--- app/src/main/java/com/farhad/visitingcard/MainActivity.java
+++ app/src/main/java/com/farhad/visitingcard/MainActivity.java
@@ -2,7 +2,12 @@ package com.farhad.visitingcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+    public void onCallClick(View view) {
+
+        String phone = "+8801844991983";
+        Intent intent = new Intent(Intent.ACTION_DIAL,
+                Uri.fromParts("tel", phone, null));
+        startActivity(intent);
+
+    }
+
+
+    public void onMailClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, "imfarhad01@gmail.com");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Business Deal");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void onWebClick(View view) {
+
+        Uri uri = Uri.parse("https://www.mdfarhad.com"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+
+    }
+
+    public void onLocationClick(View view) {
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?saddr=23.694925,90.4129&daddr=23.8777249,90.2658408"));
+        startActivity(intent);
+    }
+
+    public void onWhatsAppClick(View view) {
+        String contact = "+88 01844991983"; // use country code with your phone number
+        String url = "https://api.whatsapp.com/send?phone=" + contact;
+        try {
+            PackageManager pm = getApplication().getPackageManager();
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(MainActivity.this, "Whatsapp app not installed in your phone", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
+
