91327b600ed7574ed3fa792dd3a8edf18714f158
farhadhossen <imfarhad01@gmail.com>
2022-11-16 12:35:36 +0600

    all intent added

--- app/src/main/java/com/farhad/visitingcard/MainActivity.java
+++ app/src/main/java/com/farhad/visitingcard/MainActivity.java
@@ -2,12 +2,16 @@ package com.farhad.visitingcard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,9 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onMailClick(View view) {
+        String[] addresses = {"imfarhad01@gmail.com"};
         Intent intent = new Intent(Intent.ACTION_SENDTO);
         intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_EMAIL, "imfarhad01@gmail.com");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
         intent.putExtra(Intent.EXTRA_SUBJECT, "Business Deal");
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
@@ -67,6 +72,40 @@ public class MainActivity extends AppCompatActivity {
         } catch (PackageManager.NameNotFoundException e) {
             Toast.makeText(MainActivity.this, "Whatsapp app not installed in your phone", Toast.LENGTH_SHORT).show();
             e.printStackTrace();
+        }
+    }
+
+    public void onLinkedInClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("linkedin://add/%@" + "estoyfarhad"));
+        final PackageManager packageManager = getApplicationContext().getPackageManager();
+        final List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        if (list.isEmpty()) {
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.linkedin.com/profile/view?id=" + "estoyfarhad"));
+        }
+        startActivity(intent);
+    }
+
+    public void onGithubClick(View view) {
+        Uri uri = Uri.parse("https://github.com/farhadhossen"); // missing 'http://' will cause crashed
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void onFacebookClick(View view) {
+        Intent facebookIntent = getOpenFacebookIntent(MainActivity.this);
+        startActivity(facebookIntent);
+    }
+
+    public static Intent getOpenFacebookIntent(Context context) {
+
+        try {
+            context.getPackageManager()
+                    .getPackageInfo("com.facebook.katana", 0); //Checks if FB is even installed.
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("fb://profile/estoyfarhad")); //Trys to make intent with FB's URI
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.facebook.com/estoyfarhad")); //catches and opens a url to the desired page
         }
     }
 }
